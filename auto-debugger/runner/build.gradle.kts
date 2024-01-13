5f070b1d799b5d1024c783c95d5f289e38300f9e
leskovde <leskovar.denis@seznam.cz>
2024-01-13 16:25:22 +0100

    Fix multiproject structure - move Main into a separate runner

--- auto-debugger/runner/build.gradle.kts
+++ auto-debugger/runner/build.gradle.kts
@@ -10,7 +10,8 @@ repositories {
 }
 
 dependencies {
-    implementation(project(mapOf("path" to ":runner")))
+    implementation(project(mapOf("path" to ":instrumentor-common")))
+    implementation(project(mapOf("path" to ":instrumentor-java")))
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
