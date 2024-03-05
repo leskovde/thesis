2df0ec6de80f10ee1fdf3f752bc2eb9834d55dd6
leskovde <leskovar.denis@seznam.cz>
2024-03-05 10:03:05 +0100

    Update class names

--- auto-debugger/instrumentor-common/src/main/java/cz/cuni/mff/d3s/autodebugger/instrumentor/common/BytecodeInstrumentor.java
+++
@@ -1,7 +0,0 @@
-package cz.cuni.mff.d3s.autodebugger.instrumentor.common;
-
-public interface BytecodeInstrumentor {
-    String loadBytecode(String filePath);
-    String instrumentCode(String bytecode);
-    void saveBytecode(String instrumentedBytecode, String filePath);
-}
--- auto-debugger/runner/src/main/java/cz/cuni/mff/d3s/autodebugger/runner/Main.java
+++ auto-debugger/runner/src/main/java/cz/cuni/mff/d3s/autodebugger/runner/Main.java
@@ -1,20 +1,17 @@
 package cz.cuni.mff.d3s.autodebugger.runner;
 
-import cz.cuni.mff.d3s.autodebugger.instrumentor.common.BytecodeInstrumentor;
-import cz.cuni.mff.d3s.autodebugger.instrumentor.java.JavaBytecodeInstrumentor;
+import cz.cuni.mff.d3s.autodebugger.instrumentor.java.DiSLInstrumentor;
 
 public class Main {
   public static void main(String[] args) {
-    if (args.length != 2) {
-      System.out.println("Please provide program path and procedure as arguments.");
-      return;
-    }
-    Arguments arguments = new Arguments(args[0], args[1]);
+//    if (args.length != 2) {
+//      System.out.println("Please provide program path and procedure as arguments.");
+//      return;
+//    }
+//    Arguments arguments = new Arguments(args[0], args[1]);
 
     // TODO: Select the implementation based on the language
-    BytecodeInstrumentor instrumentor = new JavaBytecodeInstrumentor();
-    String bytecode = instrumentor.loadBytecode(arguments.getProgramPath());
-    String instrumentedBytecode = instrumentor.instrumentCode(bytecode);
-    instrumentor.saveBytecode(instrumentedBytecode, arguments.getProgramPath());
+    DiSLInstrumentor instrumentor = DiSLInstrumentor.builder().build();
+    instrumentor.runInstrumentation();
   }
 }
