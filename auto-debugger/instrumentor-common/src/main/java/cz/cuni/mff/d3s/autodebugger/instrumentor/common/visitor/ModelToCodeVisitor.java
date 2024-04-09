49b8e35b9a314e314eeb0e18dfa112cfd65a7250
leskovde <leskovar.denis@seznam.cz>
2024-04-09 23:04:00 +0200

    Add double dispatch visitor

---
+++ auto-debugger/instrumentor-common/src/main/java/cz/cuni/mff/d3s/autodebugger/instrumentor/common/visitor/ModelToCodeVisitor.java
@@ -0,0 +1,26 @@
+package cz.cuni.mff.d3s.autodebugger.instrumentor.common.visitor;
+
+import cz.cuni.mff.d3s.autodebugger.instrumentor.common.modelling.Metaclass;
+import cz.cuni.mff.d3s.autodebugger.instrumentor.common.modelling.Model;
+
+public abstract class ModelToCodeVisitor implements ModelVisitor {
+    protected final StringBuilder stringBuilder = new StringBuilder();
+
+    @Override
+    public void visit(Model visitable) {
+        visitable.accept(this);
+    }
+
+    @Override
+    public void visit(Metaclass visitable) {
+        visitable.accept(this);
+    }
+
+    public String getCode() {
+        return stringBuilder.toString();
+    }
+
+    protected void append(String string) {
+        stringBuilder.append(string);
+    }
+}
---
+++ auto-debugger/instrumentor-common/src/main/java/cz/cuni/mff/d3s/autodebugger/instrumentor/common/visitor/ModelVisitor.java
@@ -0,0 +1,9 @@
+package cz.cuni.mff.d3s.autodebugger.instrumentor.common.visitor;
+
+import cz.cuni.mff.d3s.autodebugger.instrumentor.common.modelling.Metaclass;
+import cz.cuni.mff.d3s.autodebugger.instrumentor.common.modelling.Model;
+
+public interface ModelVisitor {
+    void visit(Model visitable);
+    void visit (Metaclass visitable);
+}
