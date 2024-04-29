ebaef9b666b10cc3e541e4ef4155d3fa582c87bc
leskovde <leskovar.denis@seznam.cz>
2024-04-29 22:58:55 +0200

    Remove visitors

--- auto-debugger/instrumentor-common/src/main/java/cz/cuni/mff/d3s/autodebugger/instrumentor/common/modelling/Extractable.java
+++
@@ -1,7 +0,0 @@
-package cz.cuni.mff.d3s.autodebugger.instrumentor.common.modelling;
-
-import cz.cuni.mff.d3s.autodebugger.instrumentor.common.visitor.ModelVisitor;
-
-public interface Extractable {
-    void accept(ModelVisitor visitor);
-}
