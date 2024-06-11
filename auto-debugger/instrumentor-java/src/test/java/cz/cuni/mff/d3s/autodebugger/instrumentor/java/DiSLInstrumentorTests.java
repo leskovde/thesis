a6dbf3ac2d94429093b59e249cf72c4e9155c33a
leskovde <leskovar.denis@seznam.cz>
2024-06-11 22:53:30 +0200

    Add IT

---
+++ auto-debugger/instrumentor-java/src/test/java/cz/cuni/mff/d3s/autodebugger/instrumentor/java/DiSLInstrumentorTests.java
@@ -0,0 +1,48 @@
+package cz.cuni.mff.d3s.autodebugger.instrumentor.java;
+
+import cz.cuni.mff.d3s.autodebugger.instrumentor.common.enums.ExportableValueType;
+import cz.cuni.mff.d3s.autodebugger.instrumentor.common.factories.IdentifierFactory;
+import cz.cuni.mff.d3s.autodebugger.instrumentor.common.identifiers.IdentifierParameters;
+import cz.cuni.mff.d3s.autodebugger.instrumentor.common.identifiers.MethodIdentifier;
+import cz.cuni.mff.d3s.autodebugger.instrumentor.common.identifiers.MethodIdentifierParameters;
+import cz.cuni.mff.d3s.autodebugger.instrumentor.common.identifiers.VariableIdentifierParameters;
+import java.nio.file.Path;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class DiSLInstrumentorTests {
+
+  @Test
+  public void givenSingleMethodWithLocalVariables_whenInstrumentingCode_thenValuesAreExtracted() {
+    // given
+    DiSLInstrumentor instrumentor =
+        DiSLInstrumentor.builder()
+            .applicationJarPath(Path.of("src/test/resources/targets/localvariables/Test.jar"))
+            .generatedCodeOutputDirectory(
+                Path.of(
+                    "../analyzer-disl/src/main/java/cz/cuni/mff/d3s/autodebugger/analyzer/disl/"))
+            .jarOutputPath(Path.of("../analyzer-disl/build/libs/instrumentation.jar"))
+            .dislRepositoryPath(Path.of("../../../disl"))
+            .methods(
+                List.of(
+                    new MethodIdentifier(
+                        MethodIdentifierParameters.builder()
+                            .className("Main")
+                            .methodName("main")
+                            .returnType("void")
+                            .build())))
+            .variables(
+                List.of(
+                    IdentifierFactory.createFrom(
+                        new IdentifierParameters(
+                            VariableIdentifierParameters.builder()
+                                .variableType("int")
+                                .variableName("a")
+                                .exportableType(ExportableValueType.VARIABLE)
+                                .build()))))
+            .build();
+
+    // when
+    var resultPaths = instrumentor.runInstrumentation();
+  }
+}
