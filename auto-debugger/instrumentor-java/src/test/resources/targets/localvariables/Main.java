e6d9a87c4a623bdc389edb1007bc7659a835bc9b
leskovde <leskovar.denis@seznam.cz>
2024-07-07 20:31:37 +0200

    Update test resources

--- auto-debugger/instrumentor-java/src/test/resources/targets/localvariables/Main.java
+++ auto-debugger/instrumentor-java/src/test/resources/targets/localvariables/Main.java
@@ -1,7 +1,17 @@
 public class Main {
 
-    public static void main(String[] args) {
-        int a = 42;
-        System.out.println(a);
-    }
+  public static int testMul(int x, int y) {
+    System.out.println("app: Main.testMul()");
+    int a = x;
+    int b = y;
+    int mul = a * b;
+    System.out.println("app: a=" + a);
+    System.out.println("app: b=" + b);
+    System.out.println("app: mul=" + mul);
+    return mul;
+  }
+
+  public static void main(String[] args) {
+    testMul(3, 5);
+  }
 }
