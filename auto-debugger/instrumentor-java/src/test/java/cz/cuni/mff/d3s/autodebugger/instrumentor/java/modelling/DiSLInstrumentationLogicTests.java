67c570d95ea7f37667788ac15be9faab16a362e6
leskovde <leskovar.denis@seznam.cz>
2024-08-04 21:58:56 +0200

    Add ShadowVM test

--- auto-debugger/instrumentor-java/src/test/java/cz/cuni/mff/d3s/autodebugger/instrumentor/java/modelling/DiSLInstrumentationLogicTests.java
+++ auto-debugger/instrumentor-java/src/test/java/cz/cuni/mff/d3s/autodebugger/instrumentor/java/modelling/DiSLInstrumentationLogicTests.java
@@ -1,20 +1,40 @@
 package cz.cuni.mff.d3s.autodebugger.instrumentor.java.modelling;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class DiSLInstrumentationLogicTests {
 
-    @Test
-    public void givenValidInstrumentationLogic_whenGeneratingCode_thenCodeIsGenerated() {
-        // given
-        DiSLInstrumentationLogic logic = new DiSLInstrumentationLogic(Constants.instrumentationLogicMethodIdentifier, Constants.dislAnnotation, Constants.instrumentationLogicExports);
+  @Test
+  public void givenValidSerializationInstrumentationLogic_whenGeneratingCode_thenCodeIsGenerated() {
+    // given
+    DiSLInstrumentationLogic logic =
+        new SerializationDiSLInstrumentationLogic(
+            Constants.instrumentationLogicMethodIdentifier,
+            Constants.dislAnnotation,
+            Constants.instrumentationLogicExports);
 
-        // when
-        String code = logic.emitCode();
+    // when
+    String code = logic.emitCode();
 
-        // then
-        assertEquals("", code);
-    }
+    // then
+    assertEquals("", code);
+  }
+
+  @Test
+  public void givenValidShadowInstrumentationLogic_whenGeneratingCode_thenCodeIsGenerated() {
+    // given
+    DiSLInstrumentationLogic logic =
+        new ShadowDiSLInstrumentationLogic(
+            Constants.instrumentationLogicMethodIdentifier,
+            Constants.dislAnnotation,
+            Constants.instrumentationLogicExports);
+
+    // when
+    String code = logic.emitCode();
+
+    // then
+    assertEquals("", code);
+  }
 }
