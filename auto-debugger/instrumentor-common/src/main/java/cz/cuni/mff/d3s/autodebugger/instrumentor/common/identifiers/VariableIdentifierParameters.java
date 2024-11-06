a939b6351c625ff447e6eb95a4ad567571e26018
leskovde <leskovar.denis@seznam.cz>
2024-11-06 14:54:55 +0100

    [Separate exportable identifiers] Rename ExportableValueType to ValueType to avoid naming conflicts

--- auto-debugger/instrumentor-common/src/main/java/cz/cuni/mff/d3s/autodebugger/instrumentor/common/identifiers/VariableIdentifierParameters.java
+++ auto-debugger/instrumentor-common/src/main/java/cz/cuni/mff/d3s/autodebugger/instrumentor/common/identifiers/VariableIdentifierParameters.java
@@ -1,11 +1,11 @@
 package cz.cuni.mff.d3s.autodebugger.instrumentor.common.identifiers;
 
-import cz.cuni.mff.d3s.autodebugger.instrumentor.common.enums.ExportableValueType;
+import cz.cuni.mff.d3s.autodebugger.instrumentor.common.enums.ValueType;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder
 public class VariableIdentifierParameters extends ExportableValueIdentifierParameters {
     {
-        exportableType = ExportableValueType.VARIABLE;
+        exportableType = ValueType.VARIABLE;
     }
 }
