9e5fd852a82b82bfbbdfc3329e27b5bef51eef12
leskovde <leskovar.denis@seznam.cz>
2024-11-20 18:32:15 +0100

    Serialize and deserialize identifier mapping

--- auto-debugger/instrumentor-common/src/main/java/cz/cuni/mff/d3s/autodebugger/instrumentor/common/Instrumentor.java
+++ auto-debugger/instrumentor-common/src/main/java/cz/cuni/mff/d3s/autodebugger/instrumentor/common/Instrumentor.java
@@ -3,6 +3,10 @@ package cz.cuni.mff.d3s.autodebugger.instrumentor.common;
 import cz.cuni.mff.d3s.autodebugger.model.java.identifiers.ExportableValue;
 import cz.cuni.mff.d3s.autodebugger.model.java.identifiers.Identifier;
 import cz.cuni.mff.d3s.autodebugger.model.java.identifiers.MethodIdentifier;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
@@ -10,9 +14,11 @@ import java.util.Map;
 
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
+import lombok.extern.slf4j.Slf4j;
 
 @SuperBuilder
 @Getter
+@Slf4j
 public abstract class Instrumentor {
   protected final Path applicationJarPath;
   protected final List<Path> classpath;
@@ -22,12 +28,31 @@ public abstract class Instrumentor {
 
   public abstract List<Path> runInstrumentation();
 
-  public String serializeIdentifiers(Path outputDirectory) {
+  public Path serializeIdentifiers(Path outputDirectory) {
     Map<Integer, ExportableValue> identifierMapping = new HashMap<>();
     for (ExportableValue value : exportedValues) {
       identifierMapping.put(value.getInternalId(), value);
     }
 
-    // TODO: Serialize to json
+    try {
+      if (!outputDirectory.toFile().exists()) {
+        if (outputDirectory.toFile().mkdirs()) {
+            log.info("Created directory {}", outputDirectory);
+            } else {
+            log.error("Failed to create directory {}", outputDirectory);
+            return null;
+        }
+      }
+      File mappingFile =
+          File.createTempFile("identifierMapping", ".json", outputDirectory.toFile());
+      try (FileOutputStream fileOutput = new FileOutputStream(mappingFile);
+          ObjectOutputStream objectStream = new ObjectOutputStream(fileOutput)) {
+        objectStream.writeObject(identifierMapping);
+      }
+      return mappingFile.toPath();
+    } catch (IOException e) {
+      log.error("Failed to serialize identifier mapping", e);
+      return null;
+    }
   }
 }
--- auto-debugger/test-generator-java/src/main/java/cz/cuni/mff/d3s/autodebugger/testgenerator/java/trace/TraceBasedUnitTestGenerator.java
+++ auto-debugger/test-generator-java/src/main/java/cz/cuni/mff/d3s/autodebugger/testgenerator/java/trace/TraceBasedUnitTestGenerator.java
@@ -3,26 +3,40 @@ package cz.cuni.mff.d3s.autodebugger.testgenerator.java.trace;
 import cz.cuni.mff.d3s.autodebugger.model.java.identifiers.ExportableValue;
 import cz.cuni.mff.d3s.autodebugger.testgenerator.common.TraceBasedGenerator;
 import cz.cuni.mff.d3s.autodebugger.testgenerator.common.UnitTestGenerator;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class TraceBasedUnitTestGenerator implements TraceBasedGenerator, UnitTestGenerator {
   private final Map<Integer, ExportableValue> identifierMapping;
 
   public TraceBasedUnitTestGenerator(Path identifierMappingFile) {
-    // TODO: deserialize JSON
+    log.info("Loading identifier mapping from file: {}", identifierMappingFile);
+    try {
+      try (FileInputStream fileReader = new FileInputStream(identifierMappingFile.toFile());
+          ObjectInputStream objectStream = new ObjectInputStream(fileReader)) {
+        identifierMapping = (HashMap<Integer, ExportableValue>) objectStream.readObject();
+      }
+    } catch (Exception e) {
+      log.error("Failed to load identifier mapping", e);
+      throw new RuntimeException("Failed to load identifier mapping", e);
+    }
   }
 
   @Override
   public List<Path> generateTests(cz.cuni.mff.d3s.autodebugger.model.java.Trace trace) {
-    System.err.println("Generating unit tests from trace: " + trace);
+    log.info("Generating unit tests from trace: {}", trace);
     return List.of();
   }
 
   @Override
   public List<Path> generateUnitTests() {
-    System.err.println("Generating unit tests");
+    log.info("Generating unit tests");
     return List.of();
   }
 }
