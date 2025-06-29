0b118860553e88034f0c0d6325ef89e333c06732
leskovde <leskovar.denis@seznam.cz>
2025-06-29 19:37:38 +0200

    Switch to .toml version catalog, use intelliJPlatform 2.6.1.-SNAPSHOT for fixed platform resolving

--- auto-debugger/settings.gradle.kts
+++ auto-debugger/settings.gradle.kts
@@ -10,14 +10,10 @@ include("analyzer-common")
 include("analyzer-java")
 include("intellij-plugin")
 
-dependencyResolutionManagement {
-
-    versionCatalogs {
-        create("libs") {
-            library("log4j", "org.apache.logging.log4j:log4j-slf4j2-impl:2.23.1")
-            library("javatuples", "org.javatuples:javatuples:1.2")
-            library("junit5", "org.junit.jupiter:junit-jupiter:5.10.2")
-            library("junit5-bom", "org.junit:junit-bom:5.10.2")
-        }
+pluginManagement {
+    repositories {
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+        gradlePluginPortal()
     }
 }
+
