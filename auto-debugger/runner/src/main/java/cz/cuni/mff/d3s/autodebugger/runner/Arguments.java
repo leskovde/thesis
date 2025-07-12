9e48a6129c55003d08a8976136c17e76d9271825
leskovde <leskovar.denis@seznam.cz>
2025-07-12 23:54:23 +0200

    Update cli arguments to match the plugin

--- auto-debugger/runner/src/main/java/cz/cuni/mff/d3s/autodebugger/runner/Arguments.java
+++ auto-debugger/runner/src/main/java/cz/cuni/mff/d3s/autodebugger/runner/Arguments.java
@@ -1,22 +1,23 @@
 package cz.cuni.mff.d3s.autodebugger.runner;
 
 import picocli.CommandLine;
+import java.util.List;
 
 public class Arguments {
     @CommandLine.Option(names = { "-j", "--jar" }, paramLabel = "JAR", description = "Path to the application JAR file", required = true)
     public String applicationJarPath;
 
-    @CommandLine.Option(names = { "-r", "--return" }, paramLabel = "RETURN", description = "Return value of the method to be analyzed", required = true)
-    public String methodReturnValue;
+    @CommandLine.Option(names = { "-s", "--source" }, paramLabel = "SOURCE", description = "Path to the target application's source code", required = true)
+    public String sourceCodePath;
 
-    @CommandLine.Option(names = { "-c", "--class" }, paramLabel = "CLASS", description = "Class containing the method to be analyzed", required = true)
-    public String methodClassName;
+    @CommandLine.Option(names = { "-m", "--method" }, paramLabel = "METHOD", description = "Target method reference (e.g., org.example.Main.main(String[]))", required = true)
+    public String targetMethodReference;
 
-    @CommandLine.Option(names = { "-m", "--method" }, paramLabel = "METHOD", description = "Method to be analyzed", required = true)
-    public String methodName;
+    @CommandLine.Option(names = { "-p", "--parameters" }, paramLabel = "PARAMETERS", description = "Target method parameters (format: type:name or slot:type)", split = ",")
+    public List<String> targetParameters;
 
-    @CommandLine.Parameters(paramLabel = "TYPES", description = "Method parameter types")
-    public String[] parameterTypes;
+    @CommandLine.Option(names = { "-f", "--fields" }, paramLabel = "FIELDS", description = "Target class fields (format: type:name)", split = ",")
+    public List<String> targetFields;
 
     @CommandLine.Option(names = { "-h", "--help" }, usageHelp = true, description = "display a help message")
     private boolean helpRequested = false;
