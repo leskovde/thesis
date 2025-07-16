72977ecc2b4c07bb775584a989983edd34619c20
leskovde <leskovar.denis@seznam.cz>
2025-07-16 23:10:08 +0200

    Create model-common

---
+++ auto-debugger/model-common/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java-library")
+    alias(libs.plugins.lombok)
+}
+
+group = "cz.cuni.mff.d3s"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.log4j)
+    testImplementation(libs.bundles.junit)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
