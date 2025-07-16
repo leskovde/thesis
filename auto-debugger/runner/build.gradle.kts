8b06e237365bede1752b2d9bf162671ee84f40bc
leskovde <leskovar.denis@seznam.cz>
2025-07-16 23:11:14 +0200

    Add references to model-common

--- auto-debugger/runner/build.gradle.kts
+++ auto-debugger/runner/build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 }
 
 dependencies {
+    implementation(project(mapOf("path" to ":model-common")))
     implementation(project(mapOf("path" to ":model-java")))
     implementation(project(mapOf("path" to ":instrumentor-common")))
     implementation(project(mapOf("path" to ":instrumentor-java")))
