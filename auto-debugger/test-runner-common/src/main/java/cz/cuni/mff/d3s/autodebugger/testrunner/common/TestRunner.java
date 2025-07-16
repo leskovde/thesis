c5ff0b509a151d0c92d8d0acb2804f9c1b816413
leskovde <leskovar.denis@seznam.cz>
2025-07-16 21:41:16 +0200

    Pass run config to the TestRunner

--- auto-debugger/test-runner-common/src/main/java/cz/cuni/mff/d3s/autodebugger/testrunner/common/TestRunner.java
+++ auto-debugger/test-runner-common/src/main/java/cz/cuni/mff/d3s/autodebugger/testrunner/common/TestRunner.java
@@ -1,5 +1,7 @@
 package cz.cuni.mff.d3s.autodebugger.testrunner.common;
 
+import cz.cuni.mff.d3s.autodebugger.model.java.RunConfiguration;
+
 import java.nio.file.Path;
 import java.util.List;
 
@@ -9,23 +11,30 @@ import java.util.List;
  * detailed execution traces for debugging purposes.
  */
 public interface TestRunner {
-    
+
+    /**
+     * Configures the test runner with the given run configuration.
+     *
+     * @param configuration The run configuration containing execution parameters
+     */
+    void configure(RunConfiguration configuration);
+
     /**
      * Executes a test suite from the given test files.
-     * 
+     *
      * @param testFiles List of paths to test files to execute
      * @return TestExecutionResult containing the results and traces
      */
     TestExecutionResult executeTests(List<Path> testFiles);
-    
+
     /**
      * Executes a single test file.
-     * 
+     *
      * @param testFile Path to the test file to execute
      * @return TestExecutionResult containing the results and traces
      */
     TestExecutionResult executeTest(Path testFile);
-    
+
     /**
      * Configures the test execution environment.
      * 
