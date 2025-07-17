414c0256db857becf918e4aa23bfca708c915262
leskovde <leskovar.denis@seznam.cz>
2025-07-17 21:00:01 +0200

    Add anthropic java api

--- auto-debugger/test-generator-java/build.gradle.kts
+++ auto-debugger/test-generator-java/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation(project(mapOf("path" to ":model-common")))
     implementation(project(mapOf("path" to ":model-java")))
     implementation(libs.log4j)
+    implementation("com.anthropic:anthropic-java:2.1.0")
     testImplementation(libs.bundles.junit)
 }
 
