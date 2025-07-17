ee5a86bc04f020ad3f04923a0225e92f67ea40df
leskovde <leskovar.denis@seznam.cz>
2025-07-17 10:24:36 +0200

    Update Instrumentor interface

--- auto-debugger/instrumentor-common/src/main/java/cz/cuni/mff/d3s/autodebugger/instrumentor/common/Instrumentor.java
+++ auto-debugger/instrumentor-common/src/main/java/cz/cuni/mff/d3s/autodebugger/instrumentor/common/Instrumentor.java
@@ -1,58 +1,11 @@
 package cz.cuni.mff.d3s.autodebugger.instrumentor.common;
 
-import cz.cuni.mff.d3s.autodebugger.model.common.identifiers.ExportableValue;
-import cz.cuni.mff.d3s.autodebugger.model.java.identifiers.Identifier;
-import cz.cuni.mff.d3s.autodebugger.model.common.identifiers.MethodIdentifier;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import cz.cuni.mff.d3s.autodebugger.instrumentor.common.modelling.InstrumentationModel;
+
 import java.nio.file.Path;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import lombok.Getter;
-import lombok.experimental.SuperBuilder;
-import lombok.extern.slf4j.Slf4j;
+public interface Instrumentor {
 
-@SuperBuilder
-@Getter
-@Slf4j
-public abstract class Instrumentor {
-  protected final Path applicationJarPath;
-  protected final List<Path> classpath;
-  protected final Identifier className;
-  protected final List<ExportableValue> exportedValues;
-  protected final MethodIdentifier method;
-
-  public abstract List<Path> runInstrumentation();
-
-  public Path serializeIdentifiers(Path outputDirectory) {
-    Map<Integer, ExportableValue> identifierMapping = new HashMap<>();
-    for (ExportableValue value : exportedValues) {
-      identifierMapping.put(value.getInternalId(), value);
-    }
-
-    try {
-      if (!outputDirectory.toFile().exists()) {
-        if (outputDirectory.toFile().mkdirs()) {
-            log.info("Created directory {}", outputDirectory);
-            } else {
-            log.error("Failed to create directory {}", outputDirectory);
-            return null;
-        }
-      }
-      File mappingFile =
-          File.createTempFile("identifierMapping", ".json", outputDirectory.toFile());
-      try (FileOutputStream fileOutput = new FileOutputStream(mappingFile);
-          ObjectOutputStream objectStream = new ObjectOutputStream(fileOutput)) {
-        objectStream.writeObject(identifierMapping);
-      }
-      return mappingFile.toPath();
-    } catch (IOException e) {
-      log.error("Failed to serialize identifier mapping", e);
-      return null;
-    }
-  }
+    List<Path> generateInstrumentation(InstrumentationModel model);
 }
