37c59767ce1ab5a4ae85d16aa7657a8935d24cbc
leskovde <leskovar.denis@seznam.cz>
2025-07-17 10:23:23 +0200

    Refactor DiSLInstrumentor, move running of the instrumentation out

--- auto-debugger/instrumentor-java/src/main/java/cz/cuni/mff/d3s/autodebugger/instrumentor/java/DiSLCompiler.java
+++ auto-debugger/instrumentor-java/src/main/java/cz/cuni/mff/d3s/autodebugger/instrumentor/java/DiSLCompiler.java
@@ -17,132 +17,131 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @AllArgsConstructor
 public class DiSLCompiler {
-  private final String DISL_CLASS_NAME = "DiSLClass";
-  // private final List<String> JAVAC_OPTIONS = List.of("-proc:only");
-  private DiSLInstrumentor instrumentor;
+    private final String DISL_CLASS_NAME = "DiSLClass";
 
-  public Optional<Path> compileDiSLClass(Path instrumentationSourcePath, List<Path> appClasspath) {
-    // TODO: Maybe via Gradle?
-    //  https://stackoverflow.com/questions/49876189/how-to-run-a-gradle-task-from-a-java-code
-    try {
-      log.info("Compiling DiSL class");
-      File instrumentationSourceFile = instrumentationSourcePath.toFile();
-      String fileName = DISL_CLASS_NAME + ".java";
-      File dislClassFile = new File(instrumentationSourceFile, fileName);
-      File collectorClassFile = new File(instrumentationSourceFile, "Collector.java");
-      File collectorReClassFile = new File(instrumentationSourceFile, "CollectorRe.java");
-      JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-      try (StandardJavaFileManager fileManager =
-          compiler.getStandardFileManager(null, null, null)) {
-        File outputDirectory = new File("../analyzer-disl/build/classes/java/main/");
+    private final Path jarOutputPath;
+    private final Path dislClasspathRoot;
+    private final List<Path> appClasspathEntries;
+
+    public Optional<Path> compileDiSLClass(Path instrumentationSourcePath) {
         try {
-          if (outputDirectory.mkdirs()) {
-            log.info("Created output directory");
-          }
+            log.info("Compiling DiSL class");
+            File instrumentationSourceFile = instrumentationSourcePath.toFile();
+            String fileName = DISL_CLASS_NAME + ".java";
+            File dislClassFile = new File(instrumentationSourceFile, fileName);
+            File collectorClassFile = new File(instrumentationSourceFile, "Collector.java");
+            File collectorReClassFile = new File(instrumentationSourceFile, "CollectorRe.java");
+            JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+            try (StandardJavaFileManager fileManager =
+                         compiler.getStandardFileManager(null, null, null)) {
+                File outputDirectory = new File("../analyzer-disl/build/classes/java/main/");
+                try {
+                    if (outputDirectory.mkdirs()) {
+                        log.info("Created output directory");
+                    }
+                } catch (Exception e) {
+                    log.error("Failed to create output directory", e);
+                    return Optional.empty();
+                }
+                fileManager.setLocation(StandardLocation.CLASS_OUTPUT, List.of(outputDirectory));
+                fileManager.setLocation(StandardLocation.CLASS_PATH, getDiSLClassPath(appClasspathEntries));
+                Iterable<? extends JavaFileObject> compilationUnits =
+                        fileManager.getJavaFileObjectsFromFiles(
+                                List.of(dislClassFile, collectorClassFile, collectorReClassFile));
+                if (!compiler.getTask(null, fileManager, null, null, null, compilationUnits).call()) {
+                    log.error("Failed to compile DiSL class");
+                    return Optional.empty();
+                }
+                return packageObjects(fileManager);
+            }
         } catch (Exception e) {
-          log.error("Failed to create output directory", e);
-          return Optional.empty();
+            log.error("Failed to compile DiSL class", e);
+            return Optional.empty();
         }
-        fileManager.setLocation(StandardLocation.CLASS_OUTPUT, List.of(outputDirectory));
-        fileManager.setLocation(StandardLocation.CLASS_PATH, getDiSLClassPath(appClasspath));
-        Iterable<? extends JavaFileObject> compilationUnits =
-            fileManager.getJavaFileObjectsFromFiles(
-                List.of(dislClassFile, collectorClassFile, collectorReClassFile));
-        if (!compiler.getTask(null, fileManager, null, null, null, compilationUnits).call()) {
-          log.error("Failed to compile DiSL class");
-          return Optional.empty();
-        }
-        return packageObjects(fileManager);
-      }
-    } catch (Exception e) {
-      log.error("Failed to compile DiSL class", e);
-      return Optional.empty();
     }
-  }
 
-  private Optional<Path> packageObjects(StandardJavaFileManager fileManager) {
-    Manifest manifest = new Manifest();
-    manifest.getMainAttributes().put(Attributes.Name.MANIFEST_VERSION, "1.0");
-    manifest.getMainAttributes().putValue("DiSL-Classes", "DiSLClass");
-    try {
-      if (instrumentor.getJarOutputPath().toFile().exists()) {
-        if (instrumentor.getJarOutputPath().toFile().delete()) {
-          log.info("Removed old JAR file");
+    private Optional<Path> packageObjects(StandardJavaFileManager fileManager) {
+        Manifest manifest = new Manifest();
+        manifest.getMainAttributes().put(Attributes.Name.MANIFEST_VERSION, "1.0");
+        manifest.getMainAttributes().putValue("DiSL-Classes", "DiSLClass");
+        try {
+            if (jarOutputPath.toFile().exists()) {
+                if (jarOutputPath.toFile().delete()) {
+                    log.info("Removed old JAR file");
+                } else {
+                    log.error("Failed to remove old JAR file");
+                    return Optional.empty();
+                }
+            }
+        } catch (Exception e) {
+            log.error("Failed to remove old JAR file", e);
+            return Optional.empty();
+        }
+        try {
+            if (jarOutputPath.toFile().getParentFile().mkdirs()) {
+                log.info("Created JAR directory");
+            }
+        } catch (Exception e) {
+            log.error("Failed to create JAR directory", e);
+            return Optional.empty();
+        }
+        try (JarOutputStream target =
+                     new JarOutputStream(
+                             new FileOutputStream(jarOutputPath.toFile()), manifest)) {
+            for (var object :
+                    fileManager.list(
+                            StandardLocation.CLASS_OUTPUT, "", Set.of(JavaFileObject.Kind.CLASS), true)) {
+                log.info("Compiled class: {}", object.getName());
+                addToJar(new File(object.getName()), target);
+            }
+        } catch (IOException e) {
+            log.error("Failed to package DiSL class", e);
+            return Optional.empty();
+        }
+        return Optional.of(jarOutputPath);
+    }
+
+    private void addToJar(File source, JarOutputStream target) throws IOException {
+        String name = source.toPath().getFileName().toString();
+        if (source.isDirectory()) {
+            //      if (!name.endsWith("/")) {
+            //        name += "/";
+            //      }
+            //      JarEntry entry = new JarEntry(name);
+            //      entry.setTime(source.lastModified());
+            //      target.putNextEntry(entry);
+            //      target.closeEntry();
+            //      for (File nestedFile : source.listFiles()) {
+            //        addToJar(nestedFile, target);
+            //      }
+            log.error("Directory in JAR not supported");
+            throw new IOException("Directory in JAR not supported");
         } else {
-          log.error("Failed to remove old JAR file");
-          return Optional.empty();
+            JarEntry entry = new JarEntry(name);
+            entry.setTime(source.lastModified());
+            target.putNextEntry(entry);
+            try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(source))) {
+                byte[] buffer = new byte[1024];
+                while (true) {
+                    int count = in.read(buffer);
+                    if (count == -1) break;
+                    target.write(buffer, 0, count);
+                }
+                target.closeEntry();
+            }
         }
-      }
-    } catch (Exception e) {
-      log.error("Failed to remove old JAR file", e);
-      return Optional.empty();
     }
-    try {
-      if (instrumentor.getJarOutputPath().toFile().getParentFile().mkdirs()) {
-        log.info("Created JAR directory");
-      }
-    } catch (Exception e) {
-      log.error("Failed to create JAR directory", e);
-      return Optional.empty();
-    }
-    try (JarOutputStream target =
-        new JarOutputStream(
-            new FileOutputStream(instrumentor.getJarOutputPath().toFile()), manifest)) {
-      for (var object :
-          fileManager.list(
-              StandardLocation.CLASS_OUTPUT, "", Set.of(JavaFileObject.Kind.CLASS), true)) {
-        log.info("Compiled class: {}", object.getName());
-        addToJar(new File(object.getName()), target);
-      }
-    } catch (IOException e) {
-      log.error("Failed to package DiSL class", e);
-      return Optional.empty();
-    }
-    return Optional.of(instrumentor.getJarOutputPath());
-  }
 
-  private void addToJar(File source, JarOutputStream target) throws IOException {
-    // String name = source.getPath().replace("\\", "/");
-    String name = source.toPath().getFileName().toString();
-    if (source.isDirectory()) {
-      //      if (!name.endsWith("/")) {
-      //        name += "/";
-      //      }
-      //      JarEntry entry = new JarEntry(name);
-      //      entry.setTime(source.lastModified());
-      //      target.putNextEntry(entry);
-      //      target.closeEntry();
-      //      for (File nestedFile : source.listFiles()) {
-      //        addToJar(nestedFile, target);
-      //      }
-      log.error("Directory in JAR not supported");
-      throw new IOException("Directory in JAR not supported");
-    } else {
-      JarEntry entry = new JarEntry(name);
-      entry.setTime(source.lastModified());
-      target.putNextEntry(entry);
-      try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(source))) {
-        byte[] buffer = new byte[1024];
-        while (true) {
-          int count = in.read(buffer);
-          if (count == -1) break;
-          target.write(buffer, 0, count);
-        }
-        target.closeEntry();
-      }
+    private List<File> getDiSLClassPath(List<Path> appClasspath) {
+        List<File> classPath = appClasspath.stream().map(Path::toFile).collect(Collectors.toList());
+        classPath.add(new File(dislClasspathRoot.toString(), "disl-server.jar"));
+        classPath.add(new File(dislClasspathRoot.toString(), "dislre-server.jar"));
+        classPath.add(new File(dislClasspathRoot.toString(), "dislre-dispatch.jar"));
+        classPath.add(
+                new File("../test-generator-java/build/libs/test-generator-java-1.0-SNAPSHOT-all.jar"));
+        classPath.add(
+                new File("../test-generator-common/build/libs/test-generator-common-1.0-SNAPSHOT.jar"));
+        classPath.add(new File("../model-java/build/libs/model-java-1.0-SNAPSHOT.jar"));
+        return classPath;
     }
-  }
-
-  private List<File> getDiSLClassPath(List<Path> appClasspath) {
-    List<File> classPath = appClasspath.stream().map(Path::toFile).collect(Collectors.toList());
-    classPath.add(new File(instrumentor.getDislClassPath().toString(), "disl-server.jar"));
-    classPath.add(new File(instrumentor.getDislClassPath().toString(), "dislre-server.jar"));
-    classPath.add(new File(instrumentor.getDislClassPath().toString(), "dislre-dispatch.jar"));
-    classPath.add(
-        new File("../test-generator-java/build/libs/test-generator-java-1.0-SNAPSHOT-all.jar"));
-    classPath.add(
-        new File("../test-generator-common/build/libs/test-generator-common-1.0-SNAPSHOT.jar"));
-    classPath.add(new File("../model-java/build/libs/model-java-1.0-SNAPSHOT.jar"));
-    return classPath;
-  }
 }
--- auto-debugger/instrumentor-java/src/main/java/cz/cuni/mff/d3s/autodebugger/instrumentor/java/DiSLInstrumentor.java
+++ auto-debugger/instrumentor-java/src/main/java/cz/cuni/mff/d3s/autodebugger/instrumentor/java/DiSLInstrumentor.java
@@ -1,104 +1,87 @@
 package cz.cuni.mff.d3s.autodebugger.instrumentor.java;
 
 import cz.cuni.mff.d3s.autodebugger.instrumentor.common.Instrumentor;
-import cz.cuni.mff.d3s.autodebugger.instrumentor.common.modelling.Model;
-import cz.cuni.mff.d3s.autodebugger.instrumentor.java.modelling.DiSLModel;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import cz.cuni.mff.d3s.autodebugger.instrumentor.common.modelling.InstrumentationModel;
+
+import java.io.*;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+
+import cz.cuni.mff.d3s.autodebugger.model.common.identifiers.ExportableValue;
+import cz.cuni.mff.d3s.autodebugger.model.java.JavaRunConfiguration;
+import cz.cuni.mff.d3s.autodebugger.model.java.helper.DiSLPathHelper;
+import cz.cuni.mff.d3s.autodebugger.model.java.identifiers.JavaClassIdentifier;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.experimental.SuperBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.javatuples.Pair;
 
 @Slf4j
-@SuperBuilder
-public class DiSLInstrumentor extends Instrumentor {
+@Builder
+public class DiSLInstrumentor implements Instrumentor {
 
-  @Builder.Default private Path dislRepositoryPath = Path.of("../../disl/");
+    private final JavaClassIdentifier instrumentationClassName;
 
-  @Builder.Default
-  private Path generatedCodeOutputDirectory =
-      Path.of("analyzer-disl/src/main/java/cz/cuni/mff/d3s/autodebugger/analyzer/disl/");
+    private final JavaRunConfiguration runConfiguration;
 
-  @Getter @Builder.Default
-  private Path jarOutputPath = Path.of("analyzer-disl/build/libs/instrumentation.jar");
+    @Builder.Default
+    private Path generatedCodeOutputDirectory =
+            Path.of("analyzer-disl/src/main/java/cz/cuni/mff/d3s/autodebugger/analyzer/disl/");
 
-  public Path getDislClassPath() {
-    return dislRepositoryPath.resolve("output/lib/");
-  }
+    @Getter @Builder.Default
+    private Path jarOutputPath = Path.of("analyzer-disl/build/libs/instrumentation.jar");
 
-  public Path getDislRunnerPath() {
-    return dislRepositoryPath.resolve("bin/disl.py");
-  }
-
-  public Path getDislHomePath() {
-    return dislRepositoryPath.resolve("output");
-  }
-
-  @Override
-  public List<Path> runInstrumentation() {
-    var identifierMapping = serializeIdentifiers(Path.of("../identifiers"));
-    var templateHandler = new JavaTemplateHandler(new JavaTemplateTransformer("${%s}"));
-    templateHandler.transformFile(
-            generatedCodeOutputDirectory.resolve("Collector.jt"),
-            generatedCodeOutputDirectory.resolve("Collector.java"),
-            Pair.with("PATH", identifierMapping.toAbsolutePath().toString()));
-    var jarPath = generateDiSLClass(new DiSLModel(this)).flatMap(this::compileDiSLClass);
-    return jarPath.map(this::instrumentApplication).orElseGet(List::of);
-  }
-
-  private List<Path> instrumentApplication(Path instrumentationJarPath) {
-    log.info("Running DiSL instrumentation");
-    try {
-      var scriptProcess =
-          new ProcessBuilder(
-                  "python3",
-                  getDislRunnerPath().toString(),
-                  "-d",
-                  getDislHomePath().toString(),
-                  "-cse",
-                  "-e_cp",
-                  "../test-generator-java/build/libs/*:../test-generator-common/build/libs/*:../model-java/build/libs/*",
-                  "--",
-                  instrumentationJarPath.toString(),
-                  "-jar",
-                  applicationJarPath.toString())
-              .start();
-      // Print stdout
-      try (var stdoutReader =
-          new BufferedReader(new InputStreamReader(scriptProcess.getInputStream()))) {
-        String line;
-        while ((line = stdoutReader.readLine()) != null) {
-          log.info(line);
-        }
-      }
-      // Print stderr
-      try (var stderrReader =
-          new BufferedReader(new InputStreamReader(scriptProcess.getErrorStream()))) {
-        String line;
-        while ((line = stderrReader.readLine()) != null) {
-          log.error(line);
-        }
-      }
-      scriptProcess.waitFor();
-      log.info("DiSL instrumentation finished");
-    } catch (Exception e) {
-      log.error("Failed to run DiSL instrumentation", e);
+    @Override
+    public List<Path> generateInstrumentation(InstrumentationModel model) {
+        var identifierMapping = serializeIdentifiers(Path.of("../identifiers"));
+        var templateHandler = new JavaTemplateHandler(new JavaTemplateTransformer("${%s}"));
+        templateHandler.transformFile(
+                generatedCodeOutputDirectory.resolve("Collector.jt"),
+                generatedCodeOutputDirectory.resolve("Collector.java"),
+                Pair.with("PATH", identifierMapping.toAbsolutePath().toString()));
+        var instrumentationJarPath = generateDiSLClass(model).flatMap(this::compileDiSLClass);
+        return List.of(instrumentationJarPath.orElseThrow());
     }
-    return List.of();
-  }
 
-  private Optional<Path> generateDiSLClass(Model model) {
-    var generator = new DiSLClassGenerator(generatedCodeOutputDirectory, model);
-    return generator.generateCode();
-  }
+    private Optional<Path> generateDiSLClass(InstrumentationModel model) {
+        var generator = new DiSLClassGenerator(generatedCodeOutputDirectory, model);
+        return generator.generateCode();
+    }
 
-  private Optional<Path> compileDiSLClass(Path instrumentationSource) {
-    var compiler = new DiSLCompiler(this);
-    return compiler.compileDiSLClass(instrumentationSource, classpath);
-  }
+    private Optional<Path> compileDiSLClass(Path instrumentationSource) {
+        var compiler = new DiSLCompiler(jarOutputPath, DiSLPathHelper.getDislClassPathRoot(runConfiguration),
+                runConfiguration.getClasspathEntries());
+        return compiler.compileDiSLClass(instrumentationSource);
+    }
+
+    private Path serializeIdentifiers(Path outputDirectory) {
+        Map<Integer, ExportableValue> identifierMapping = new HashMap<>();
+        for (ExportableValue value : runConfiguration.getExportableValues()) {
+            identifierMapping.put(value.getInternalId(), value);
+        }
+        try {
+            if (!outputDirectory.toFile().exists()) {
+                if (outputDirectory.toFile().mkdirs()) {
+                    log.info("Created directory {}", outputDirectory);
+                } else {
+                    log.error("Failed to create directory {}", outputDirectory);
+                    return null;
+                }
+            }
+            File mappingFile =
+                    File.createTempFile("identifierMapping", ".json", outputDirectory.toFile());
+
+            try (FileOutputStream fileOutput = new FileOutputStream(mappingFile);
+                 ObjectOutputStream objectStream = new ObjectOutputStream(fileOutput)) {
+                objectStream.writeObject(identifierMapping);
+            }
+            return mappingFile.toPath();
+        } catch (IOException e) {
+            log.error("Failed to serialize identifier mapping", e);
+            return null;
+        }
+    }
 }
